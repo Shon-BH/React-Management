cd8bb198f09d963d649391ae20cb9e3342957245
Shon-BH <sbhbins@gmail.com>
2022-01-03 16:01:15 +0900

    4

---
+++ src/Combobox.js
@@ -0,0 +1,26 @@
+import * as React from 'react';
+import TextField from '@mui/material/TextField';
+import Autocomplete from '@mui/material/Autocomplete';
+
+export default function ComboBox() {
+  return (
+    <Autocomplete
+      disablePortal
+      id="combo-box-demo"
+      options={top100Films}
+      sx={{ width: 165 }}
+      renderInput={(params) => <TextField {...params} label="회사" />}
+    />
+  );
+}
+
+// Combobox : 회사 이름 넣기
+const top100Films = [
+    { label: 'The Shawshank Redemption', year: 1994 },
+    { label: 'The Godfather', year: 1972 },
+    { label: 'The Godfather: Part II', year: 1974 },
+    { label: 'The Dark Knight', year: 2008 },
+    { label: '12 Angry Men', year: 1957 },
+    { label: "Schindler's List", year: 1993 },
+    { label: 'Pulp Fiction', year: 1994 },
+];
