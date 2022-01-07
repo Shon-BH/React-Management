d5d20a72d3368560561ea1b750100c723c71cf31
Programmer-SON <sjw3957@gmail.com>
2022-01-08 07:15:38 +0900

    생산계획등록 서버 api 연동 추가

--- src/NestedGrid.js
+++ src/NestedGrid.js
@@ -19,6 +19,7 @@ import ComboBoxProduct from './comboboxes/ComboBoxProduct';
 
 import FormatListBulletedIcon from '@mui/icons-material/FormatListBulleted';
 import WebAssetIcon from '@mui/icons-material/WebAsset';
+import axios from 'axios';
 
 const Item = styled(Paper)(({ theme }) => ({
   ...theme.typography.body2,
@@ -27,7 +28,7 @@ const Item = styled(Paper)(({ theme }) => ({
   color: theme.palette.text.secondary,
 }));
 
-function BasicDatePicker() {
+function BasicDatePicker({setDate}) {
     const [value, setValue] = React.useState(null);
     
     return (
@@ -37,6 +38,7 @@ function BasicDatePicker() {
           value={value}
           onChange={(newValue) => {
             setValue(newValue);
+            setDate(newValue);
           }}
           renderInput={(params) => <TextField {...params} />}
         />
@@ -44,11 +46,11 @@ function BasicDatePicker() {
     );
 }
 
-function FirstFormRow(props) {
+function FirstFormRow({setCompany}) {
   return (
     <React.Fragment>
       <Grid item xs={2}>
-          <Item><ComboBoxCompany/></Item>
+          <Item><ComboBoxCompany setCompany={setCompany}/></Item>
         </Grid>
           
         <Grid item xs={2}>
@@ -106,24 +108,27 @@ function SecondFormRow() {
     );
   }
 
-  function ThirdFormRow() {
+  function ThirdFormRow({setProcessStart, setProcessEnd, setProductId, setStockPlan}) {
     return (
       <React.Fragment>
         
           <Grid item xs={2}>
-            <Item><BasicDatePicker/></Item>
+            <Item><BasicDatePicker setDate={setProcessStart}/></Item>
           </Grid>
           <Grid item xs={2}>
-            <Item><BasicDatePicker/></Item>
+            <Item><BasicDatePicker setDate={setProcessEnd}/></Item>
           </Grid>
           <Grid item xs={2}>
-            <Item><ComboBoxProduct/></Item>
+            <Item><ComboBoxProduct setProductId={setProductId}/></Item>
           </Grid>
           <Grid item xs={2}>
             <Item>
               <TextField id="outlined-basic" 
                           label="수량" 
-                          variant="outlined" 
+                          variant="outlined"
+                          onChange={(e)=>{
+                            setStockPlan(e.target.value);
+                          }}
                 />
              </Item>
           {/* <Item>Item</Item> */}
@@ -135,48 +140,81 @@ function SecondFormRow() {
 
 export default function NestedGrid() {
 
-    const [company, setCompany] = React.useState("");
+    const [company, setCompany] = React.useState({
+      label: '',
+      companyId: '',      
+    });
 //    const [regDate, setRegDate] = React.useState("");
 //    const [officer, setOfficer] = React.useState("");
 //    const [email, setEmail] = React.useState("");
 //    const [phoneNum, setphoneNum] = React.useState("");
-    const [processStart, setprocessStart] = React.useState("");
-    const [processEnd, setprocessEnd] = React.useState("");
+    const [processStart, setProcessStart] = React.useState("");
+    const [processEnd, setProcessEnd] = React.useState("");
     const [productId, setProductId] = React.useState("");
+   
     const [stockPlan, setStockPlan] = React.useState("");
+    
+    const f1 = () => {
+      let jsonData = {
+        companyId : company.companyId,
+        processStart : processStart,
+        processEnd : processEnd
+      }
 
+      // axios.post('/process-service/orders', JSON.stringify(jsonData) ,{
+      //   headers: {
+      //     "Content-Type": "application/json",
+      //   },
+      // })
+      // .then(
+      //   (res) =>{console.log(res.data);}
+      // )
+      
+      // console.log(company);      
+      // console.log(processStart);
+      // console.log(processEnd);
+      // console.log(productId);
+      // console.log(stockPlan);
+      
+      console.log(typeof new Date());
+    }
+ 
 
     return (
+          
         <Box sx={{ flexGrow: 1 }}>
+          <button onClick={f1}> 버튼</button>
           <form>
+              
               <h1><WebAssetIcon />생산계획등록</h1>
               
               <Grid container spacing={1}>
-                  <Grid container item spacing={3}>
-                      <FirstFormRow />
-                  </Grid>
-                  <Grid container item spacing={3}>
-                      <SecondFormRow />
-                  </Grid>
-                  <HorizonLine />
+                <Grid container item spacing={3}>
+                    <FirstFormRow setCompany={setCompany}/>
+                </Grid>
+                <Grid container item spacing={3}>
+                    <SecondFormRow />
+                </Grid>
+                <HorizonLine />
               </Grid>
 
               {/* <input type="text" value={planData}/> */}
 
               <Grid container spacing={1}>
-              <h1><WebAssetIcon />상세정보</h1>
-                  <Grid container item spacing={3}>
-                      <ThirdFormRow />
-                  </Grid>
-                  <HorizonLine />
+                <h1><WebAssetIcon />상세정보</h1>
+                <Grid container item spacing={3}>
+                    <ThirdFormRow setProcessStart={setProcessStart} setProcessEnd={setProcessEnd} setProductId={setProductId} setStockPlan={setStockPlan}/>
+                </Grid>
+                <HorizonLine />
               </Grid>
                 
-            </form>
+            
             <h1><FormatListBulletedIcon />주문내역</h1>
                 <Table1 />
             <h1><FormatListBulletedIcon />코일정보</h1>
                 <Table2 />
-
+                
+            </form>
         </Box>
     );
 }
